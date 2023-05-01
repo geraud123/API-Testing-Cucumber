531af10e81bf38aac74d78aea3a85a1065dcc342
Arsene Kwete <tchinda.geraud@yahoo.fr>
2023-05-01 09:19:33 +0200

    Add Hooks in the runner

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -2,17 +2,19 @@ package runner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import myHooks.Hooks;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "step_def",
+        glue = {"step_def", "myHooks"},
         dryRun = true,
         plugin = {"json:target/cucumber-report/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt"
-        }
+        },
+        tags = ""
 )
 public class CukesRunner {
 }
