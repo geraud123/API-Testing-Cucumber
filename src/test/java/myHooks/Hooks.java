881cd5f9a07842cf04a51bdaf432d0e47cd144c4
Arsene Kwete <tchinda.geraud@yahoo.fr>
2023-05-01 09:17:56 +0200

    configure Hooks file

--- src/test/java/myHooks/Hooks.java
+++ src/test/java/myHooks/Hooks.java
@@ -2,19 +2,24 @@ package myHooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import utilities.Driver;
+import io.restassured.RestAssured;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import utilities.ConfigReader;
 
 public class Hooks {
 
+    private static final Logger LOG = LogManager.getLogger(Hooks.class);
+    private static final String BASE_URI = ConfigReader.getProperty("baseURI");
 
     @Before
-    public void setup(){
-        System.out.println("launch Simple Books API");
+    public void setup() {
+        RestAssured.baseURI = BASE_URI;
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         //Driver.closeDriver();
-        System.out.println("close the Driver");
+        LOG.info("close the Driver");
     }
 }
